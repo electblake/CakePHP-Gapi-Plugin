314cf611cef1e72b1ee47edea0ce4ab0171b6eac
lokiracer <lokiracer@gmail.com>
2013-03-27 15:08:59 +0100

    Added CakePHP caching and profile_id usage in key
    Removed the custom caching code and replaced with CakePHP cache class usage.
    
    Added profile_id to the cache key.  Otherwise cache was no unique per domain, but shared by all domains.

--- Controller/Component/AnalyticsComponent.php
+++ Controller/Component/AnalyticsComponent.php
@@ -15,10 +15,10 @@
  * @since         DebugKit 0.1
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
- 
+
 #App::uses('FB', 'Facebook.Lib');
 #App::uses('FacebookInfo', 'Facebook.Lib');
-App::import('Vendor', 'Gapi.gapi',
+App::import('Vendor', 'Analytics.Gapi',
   array(
     'file' => 'gapi.class.php'
   )
@@ -34,15 +34,23 @@ class AnalyticsComponent extends Component {
 	* @access public
 	*/
 	public function initialize(&$Controller, $settings = array()){
-  	Configure::load('gapi');
+  		Configure::load('gapi');
 		$this->Controller = $Controller;
 		$this->_set($settings);
 		$user = Configure::read('Gapi.email');
 		$pass = Configure::read('Gapi.password');
 		$this->profile_id = Configure::read('Gapi.profileId');
 		$this->Controller->gapi = new gapi($user, $pass);
+
+		//  configure a cache
+		Cache::config('analytics', array(
+		    'engine'   => 'File',
+		    'duration' => '+4 hours',
+		    'path'     => CACHE . 'ga' . DS,
+		    'prefix'   => ''
+		));
 	}
-	
+
 	/**
 	 * Thanks to http://snipplr.com/view/45911/ for the start
 	 */
@@ -52,70 +60,44 @@ class AnalyticsComponent extends Component {
 	                 $filter = null,
 	                 $start_date = null,
 	                 $end_date = null){
+	    $cache_id = md5($this->profile_id . serialize(func_get_args()));
 
-    $profile_id = $this->profile_id;                
-	            
-    $cache_id = md5(serialize(func_get_args()));
-    $return = false;
-    
-    // check if the cache item exists.
-    $temp_folder = TMP.'cache/ga';
-    
-    if(!is_dir($temp_folder)) { mkdir($temp_folder); }
+		$return = Cache::read($cache_id, 'analytics');
 
-		if (!is_writable(dirname($temp_folder))) {
-			trigger_error(__d('cake_dev', '"%s" directory is NOT writable.', dirname($temp_folder)), E_USER_NOTICE);
+		if(!$return) {
+			// no cache item found, so we grab it via gapi class.
+			if(!$sort) {
+				$sort = current($dimensions);
+			}
+
+			ini_set('display_errors',true);
+
+			$ga = $this->Controller->gapi;
+			$ga->requestReportData($this->profile_id, $dimensions, $metrics, $sort,$filter,$start_date,$end_date,null,100);
+
+			$return = array();
+			$return['data'] = array();
+
+			foreach($ga->getResults() as $result){
+				$data = array();
+
+				foreach($dimensions as $d){
+					$data[$d] = $result->{'get'.$d}();
+					foreach($metrics as $m){
+						$data[$m] = $result->{'get'.$m}();
+					}
+				}
+				$return['data'][] = $data;
+			}
+
+			foreach($metrics as $m){
+				$return[$m] = $ga->{'get'.$m}();
+			}
+
+			$return['timestamp'] = $ga->getUpdated();
+			Cache::write($cache_id, $return, 'analytics');
 		}
-		
 
-    $filename = $temp_folder.$cache_id;
-    
-    if(is_file($filename)){ // if cache entry exists
-      if(filemtime($filename) > (time() - 1440)){ // check if it's older than 4 hours
-        $return = unserialize(file_get_contents($filename)); // grab the cached content.
-      }
-    }
-      
-    if(!$return) {
-      // no cache item found, so we grab it via gapi class.
-      if(!$sort) {
-        $sort = current($dimensions);
-      }
-      
-      ini_set('display_errors',true);
-      
-      $ga = $this->Controller->gapi;
-      $ga->requestReportData($profile_id, $dimensions, $metrics, $sort,$filter,$start_date,$end_date,null,100);
-      
-      $return = array();
-      $return['data'] = array();
-
-      foreach($ga->getResults() as $result){
-        $data = array();
-        
-          foreach($dimensions as $d){
-            $data[$d] = (string)$result;
-              foreach($metrics as $m){
-                $data[$m] = $result->{'get'.$m}();
-              }
-          }
-          $return['data'][] = $data;
-      }
-      
-/*       $return['total'] = $ga->getTotalResults(); */
-      
-      foreach($metrics as $m){
-        $return[$m] = $ga->{'get'.$m}();
-      }
-      
-      $return['timestamp'] = $ga->getUpdated();
-    }
-      
-    // save cache item.
-    if (!file_put_contents($filename, serialize($return))) {
-      trigger_error(__d('cake_dev', 'Failed to write "%s"', $temp_folder), E_USER_NOTICE);
-    }
-    return $return;
-  }
-
+		return $return;
+	}
 }
